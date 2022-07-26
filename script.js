9840d67a7632dc0a5f2410ef4190ed8b755b5a9d
OleksandrSiedov <oleksandr.siedov@gmail.com>
2022-07-26 16:51:33 +0300

    Commit

--- script.js
+++ script.js
@@ -4,41 +4,42 @@
 
 // task #1 -  - Запитай у користувача його вік і визначи, ким він є: дитиною (0-11), підлітком (12-17), дорослим (18_59) або пенсіонером (60 ...), передбач можливість введення невірних даних.
 
-let a = Number(document.querySelector(`.age`).value);
 
-function checkAge(a) {
 
-  let b;
-  if ((a > 0) && (a <= 11)) {
-    b = 'child';
+function checkAge() {
+  let age = Number(document.querySelector(`.age`).value);
+  let group;
+  if ((age > 0) && (age <= 11)) {
+    group = 'child';
   }
-  else if ((a >= 12) && (a <= 17)) {
-    b = 'teenager';
+  else if ((age >= 12) && (age <= 17)) {
+    group = 'teenager';
   }
-  else if ((a >= 18) && (a <= 59)) {
-    b = 'adult';
+  else if ((age >= 18) && (age <= 59)) {
+    group = 'adult';
   }
-  else if ((a >= 60) && (a <= 120)) {
-    b = 'an elderly person';
+  else if ((age >= 60) && (age <= 120)) {
+    group = 'an elderly person';
   }
-  else if (a >= 120) {
-    b = 'the oldest person in the world!';
+  else if (age >= 120) {
+    group = 'the oldest person in the world!';
   }
-
-  document.querySelector(`.task1Answer`).innerHTML = b;
+  document.querySelector(`.task1Answer`).innerHTML = group;
 }
 
-// Алгоритм работает, но вывод кривой. Обсудить с Мишей
 
 //task #2 - Запитай у користувача число від 0 до 9 і виведи йому спецсимвол, який розташований на цій клавіші (1 !, 2 @, 3 # і т. д).
 
-let c = document.querySelector(`.num`).value;
+document.querySelector(`.task2 button`).onclick = checkSymbol;
 
-function checkSymboll(c) {
+function checkSymbol(e) {
+  e.preventDefault();
 
+  let c = document.querySelector(`[name='num']`).value;
   let d;
+
   switch (c) {
-    case '0':
+    case 0:
       d = ')';
       break;
     case '1':
@@ -72,6 +73,61 @@ function checkSymboll(c) {
       d = 'Incorrect data. Please enter number from 0 to 9';
       break;
   }
-}
   document.querySelector(`.task2Answer`).innerHTML = d;
+}
 
+// task 3 - Підрахуй суму всіх ЦІЛИХ (УТОЧНИЛ УСЛОВИЕ ЗАДАЧИ ИНАЧЕ ОТВЕТ БУДЕТ - БЕСКОНЕЧНОСТЬ) чисел в заданому користувачем діапазоні.
+
+function sumRange() {
+  let num1 = Number(document.querySelector(`[name="rangeNum1"]`).value);
+  let num2 = Number(document.querySelector(`[name="rangeNum2"]`).value);
+  let sum;
+  if (num1 === num2) {
+    sum = num1
+  }
+  else if (num1 > num2) {
+    sum = num2;
+    for (let i = num2; i < num1; i++) { sum = sum + i + 1; }
+  }
+  else if (num2 > num1) {
+    sum = num1;
+    for (let i = num1; i < num2; i++) { sum = sum + i + 1; }
+  }
+  document.querySelector(`.task3Answer`).innerHTML = sum;
+}
+
+// task 4 - Запитай у користувача 2 числа і знайди найбільший спільний дільник.
+
+function gcd() {
+  let num1 = Number(document.querySelector(`[name="num1"]`).value);
+  let num2 = Number(document.querySelector(`[name="num2"]`).value);
+  if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0) {
+
+    //для упростоты пока без проверок, просто считаем, что num1>num2
+    if (num1 == num2) {
+      document.querySelector(`.task4Answer`).innerHTML = num2;
+    }
+
+    else {
+
+      if (num1 < num2) {
+        [num1, num2] = [num2, num1];
+      }
+
+      while ((num1 - num2) !== num2) {
+        if ((num1 - num2) > num2) {
+          num1 = (num1 - num2)
+        } else {
+          let t = num1 - num2;
+          num1 = num2;
+          num2 = t;
+        }
+      }
+
+      document.querySelector(`.task4Answer`).innerHTML = num2;
+    }
+
+  } else {
+    document.querySelector(`.task4Answer`).innerHTML = 'ERROR! Please enter positive integer numbers';
+  }
+}
